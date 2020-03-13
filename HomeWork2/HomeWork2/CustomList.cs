4c175f31bbbf058edaa992c5d0824f49b26957d8
Zavialovserge <Zavialovserge@gmail.com>
2020-03-13 20:51:34 +0200

    Second Commit

---
+++ HomeWork2/HomeWork2/CustomList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork2
+{
+    public class CustomList<T> : IEnumerable, IEnumerator
+    {
+        private T[] _elements;
+        public int _count = 0;
+        private int _current = -1;
+        public CustomList()
+        {
+            _elements = new T[4];
+        }
+        public CustomList(int capacity)
+        {
+            _elements = new T[capacity];
+        }
+
+        public void Add(T element)
+        {
+
+            if (_count >= _elements.Length)
+            {
+                Array.Resize(ref _elements, _elements.Length * 2);
+            }
+
+            _elements[_count] = element;
+
+            ++_count;
+        }
+        public T Current => _elements[_current];
+
+        object IEnumerator.Current => Current;
+
+        public IEnumerator GetEnumerator() { return _elements.GetEnumerator(); }
+        public void RemoveAt(int index)
+        {
+            if (index < 0)
+                return;
+            if (index >= _elements.Length) return;
+
+            for (var i = index; i < _elements.Length-1; i++)
+            {
+                _elements[i] = _elements[i+1];
+            }
+
+            //_current = -1;
+        }
+
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(_elements, item);
+        }
+        public bool MoveNext()
+        {
+            ++_current;
+
+            return _current < _count;
+            //if (_count < _elements.Length - 1)
+            //{
+            //    _count++;
+            //    return true;
+            //}
+            //else
+            //{
+            //    Reset();
+            //    return false;
+            //}
+        }
+
+        public void Reset()
+        {
+            _current = -1;
+        }
+
+
+    }
+}
--- HomeWork2/HomeWork2/Program.cs
+++ HomeWork2/HomeWork2/Program.cs
@@ -6,6 +6,22 @@ namespace HomeWork2
     {
         static void Main(string[] args)
         {
+            CustomList<int> customList = new CustomList<int>() { 1, 2, 3 };
+            foreach (var item in customList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("_____________________________");
+            foreach (var item in customList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("_____________________________");
+            customList.RemoveAt(0);
+            foreach (var item in customList)
+            {
+                Console.WriteLine(item);
+            }
             Console.WriteLine("Hello World!");
         }
     }
